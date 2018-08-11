2ec9a24e861fb6dc16f40bf63d25b978e85e5098
Henri-Olivier Duche <Henri-Olivier Duche>
2018-08-11 15:49:46 +0200

    improve svg generation

--- createChordsList.js
+++ createChordsList.js
@@ -22,20 +22,27 @@ function parseChords(chords) {
     return result
 }
 
-function createSvgChord() {
+function drawSvg(shape, attributes, text=null) {
     var ns = 'http://www.w3.org/2000/svg'
-    var rect = document.createElementNS(ns, 'rect')
-    rect.setAttributeNS(null, 'rx', 20)
-    rect.setAttributeNS(null, 'ry', 20)
-    rect.setAttributeNS(null, 'x', 10)
-    rect.setAttributeNS(null, 'y', 10)
-    rect.setAttributeNS(null, 'width', 120)
-    rect.setAttributeNS(null, 'height', 150)
-    rect.setAttributeNS(null, 'style', "fill:red;stroke:black;stroke-width:5;opacity:0.5")
-    var svg = document.createElementNS(ns, 'svg')
-    svg.setAttributeNS(null, 'width', 140)
-    svg.setAttributeNS(null, 'height', 170)
-    svg.appendChild(rect)
+    var result = document.createElementNS(ns, shape);
+    for (var each in attributes) {
+        result.setAttributeNS(null, each, attributes[each]);
+    }
+    if (text) {
+        var textNode = document.createTextNode(text);
+        result.appendChild(textNode);
+    }
+    return result
+ }
+
+function createSvgChord(guitarChord) {
+    var svg = drawSvg('svg', {width:140, height:170})
+    svg.appendChild(drawSvg('rect', {rx:20, ry:20, x:10, y:10, width:120, height:150, fill:'red', stroke:'black', 'stroke-width':5, opacity:0.5}))
+    svg.appendChild(drawSvg('rect', {x:20, y:20, width:100, height:130, fill:'rgb(150,255,255)', stroke:'black'}))
+    svg.appendChild(drawSvg('circle', {cx:70, cy:120, r:10, fill:'rgb(200,50,50)', stroke:'black', 'stroke-width':2}))
+    svg.appendChild(drawSvg('line', {x1:20, y1:20, x2:120, y2:150, stroke:'rgb(200,200,200)', 'stroke-width':2}))
+    svg.appendChild(drawSvg('line', {x1:120, y1:20, x2:20, y2:150, stroke:'rgb(0,200,0)', 'stroke-width':2}))
+    svg.appendChild(drawSvg('text', {x:50, y:90, fill:'blue'}, guitarChord.firstName))
     return svg
 }
 
@@ -46,6 +53,6 @@ var htmlChordsList = parseChords(chords)
 document.getElementById("chordsUL").innerHTML = htmlChordsList
 
 for (var each of chords.guitarChords) {
-    var svg = createSvgChord()
+    var svg = createSvgChord(each)
     document.getElementById('placeHolder').appendChild(svg)
 }
