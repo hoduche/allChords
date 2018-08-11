4062001c9b6ef33fbd3dad3023653979cfbed715
Henri-Olivier Duche <Henri-Olivier Duche>
2018-08-11 11:26:36 +0200

    add svg dynamic drawings

--- createChordsList.js
+++ createChordsList.js
@@ -22,7 +22,30 @@ function parseChords(chords) {
     return result
 }
 
+function createSvgChord() {
+    var ns = 'http://www.w3.org/2000/svg'
+    var rect = document.createElementNS(ns, 'rect')
+    rect.setAttributeNS(null, 'rx', 20)
+    rect.setAttributeNS(null, 'ry', 20)
+    rect.setAttributeNS(null, 'x', 10)
+    rect.setAttributeNS(null, 'y', 10)
+    rect.setAttributeNS(null, 'width', 120)
+    rect.setAttributeNS(null, 'height', 150)
+    rect.setAttributeNS(null, 'style', "fill:red;stroke:black;stroke-width:5;opacity:0.5")
+    var svg = document.createElementNS(ns, 'svg')
+    svg.setAttributeNS(null, 'width', 140)
+    svg.setAttributeNS(null, 'height', 170)
+    svg.appendChild(rect)
+    return svg
+}
+
 var url = "http://localhost:8000/guitarChordDictionary.json"
 var chords = loadJsonFile(url)
+
 var htmlChordsList = parseChords(chords)
 document.getElementById("chordsUL").innerHTML = htmlChordsList
+
+for (var each of chords.guitarChords) {
+    var svg = createSvgChord()
+    document.getElementById('placeHolder').appendChild(svg)
+}
